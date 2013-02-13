d1be5d7e443938c82a2364dfa5e85a1253e7f34a
Panu P <panu@taguchimail.com>
2013-02-13 16:20:52 +1100

    Update description.

--- setup.py
+++ setup.py
@@ -9,6 +9,19 @@ setup(
     maintainer_email="panuph@gmail.com",
     url="https://github.com/panuph/pyfatcache",
     description="a simple python client for twitter fatcache",
+    long_description="""Example usage::
+
+        import pyfatcache
+        conn = get_conn()
+        print "expect None / get %s" % (conn.get("a"),)
+        conn.set("a", "a")
+        print "expect a / get %s" % (conn.get("a"),)
+        conn.set("a", dict(name="pyfatcache"))
+        print "expect {'name': 'pyfatcache'} / get %s" % (conn.get("a"),)
+        conn.set("a", None)
+        print "expect None / get %s" % (conn.get("a"),)
+        conn.close()
+    """,
     download_url="https://github.com/panuph/pyfatcache",
     classifiers=["Programming Language :: Python"],
     platforms=["python"],
